d97573fe6d866a21337e9c85da927e134c206882
lemonbashar <lemon.bashar@gmail.com>
2020-01-20 23:47:54 +0600

    Project Init

---
+++ src/main/java/bend/platform/BendPlatformApplication.java
@@ -0,0 +1,13 @@
+package bend.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BendPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BendPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bend/platform/BendPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package bend.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BendPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
