26685fe692cbcee273d2ae0633d4f9fc9bf8e142
lemonbashar <lemon.bashar@gmail.com>
2020-01-26 21:14:48 +0600

    Spring Boot: Bend-Platform Config module has been introduce to config platform-application fully as well.

---
+++ bend-library/config/src/main/java/bend/platform/library/config/PropertiesConfig.java
@@ -0,0 +1,25 @@
+package bend.platform.library.config;
+
+import bend.framework.properties.springproperties.SpringProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Config properties files
+ * @author lemon
+ * Email lemon.bashar@gmail.com
+ * Created 1/26/2020
+ */
+
+@Configuration
+@EnableConfigurationProperties
+public class PropertiesConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "bend-platform")
+    public SpringProperties properties() {
+        return new SpringProperties();
+    }
+}
