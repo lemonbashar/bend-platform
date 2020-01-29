29d35e98a3b44dd8cdac70707e456b1ad8781510
lemonbashar <lemon.bashar@gmail.com>
2020-01-29 23:26:04 +0600

    Some of Basic Domain's are created

--- bend-library/domain/src/main/java/bend/library/domain/BaseEntity.java
+++ bend-library/domain/src/main/java/bend/library/domain/BaseEntity.java
@@ -3,8 +3,12 @@ package bend.library.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 import java.math.BigInteger;
+import java.time.LocalDate;
 
 /**
  * @author lemon
@@ -15,5 +19,24 @@ import java.math.BigInteger;
 @Getter
 @MappedSuperclass
 public abstract class BaseEntity {
-    private BigInteger id;
+
+    @ManyToOne
+    @JoinColumn(name = "create_by")
+    private User createBy;
+
+    @ManyToOne
+    @JoinColumn(name = "update_by")
+    private User updateBy;
+
+    @Column(name = "create_date")
+    private LocalDate createDate;
+
+    @Column(name = "update_date")
+    private LocalDate updateDate;
+
+    @Column(name = "active_status")
+    private boolean active;
+
+    public abstract BigInteger getId();
+    public abstract void setId(BigInteger id);
 }
--- bend-library/domain/src/main/java/bend/library/domain/User.java
+++ bend-library/domain/src/main/java/bend/library/domain/User.java
@@ -1,14 +1,34 @@
 package bend.library.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.math.BigInteger;
 
 /**
  * @author lemon
  * Email lemon.bashar@gmail.com
  * Created 1/29/2020
  */
+@Setter
+@Getter
 @Table
 @Entity
 public class User extends BaseEntity {
+    @Id
+    private BigInteger id;
+
+    @Size(min = 5, max = 32, message = "Username length must be in between 4 ~ 32 ")
+    @Column(name = "username")
+    private String username;
+
+    @Email
+    @Column(name = "email")
+    private String email;
 }
