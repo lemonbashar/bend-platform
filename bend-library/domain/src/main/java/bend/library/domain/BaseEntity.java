f671bcdfcbe1ba201b86980babd7e1b1a52e14e9
lemonbashar <lemon.bashar@gmail.com>
2020-01-29 23:13:13 +0600

    Gradle dependency configured and domain module added

--- bend-library/domain/src/main/java/bend/library/domain/BaseEntity.java
+++ bend-library/domain/src/main/java/bend/library/domain/BaseEntity.java
@@ -1,9 +1,19 @@
 package bend.library.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import java.math.BigInteger;
+
 /**
  * @author lemon
  * Email lemon.bashar@gmail.com
  * Created 1/29/2020
  */
-public class BaseEntity {
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    private BigInteger id;
 }
---
+++ bend-library/domain/src/main/java/bend/library/domain/User.java
@@ -0,0 +1,14 @@
+package bend.library.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author lemon
+ * Email lemon.bashar@gmail.com
+ * Created 1/29/2020
+ */
+@Table
+@Entity
+public class User extends BaseEntity {
+}
