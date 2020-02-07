13a087957a0abd4d7521b8922c7f4092a40e877e
lemonbashar <lemon.bashar@gmail.com>
2020-02-07 21:02:09 +0600

    Successfully Unit-test:junit5 integration. Testing a single module or some dependent module is possible. segregated module can run application and tests.

--- bend-library/config/database/rdbms/src/main/java/bend/library/config/database/rdbms/RdbmsJpaConfig.java
+++ bend-library/config/database/rdbms/src/main/java/bend/library/config/database/rdbms/RdbmsJpaConfig.java
@@ -6,12 +6,10 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -20,7 +18,6 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.Properties;
 
