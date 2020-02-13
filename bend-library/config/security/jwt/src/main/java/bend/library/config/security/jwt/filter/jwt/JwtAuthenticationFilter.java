827e30441c2e8d0ad51c057d408deef2445e1d42
lemonbashar <lemon.bashar@gmail.com>
2020-02-14 00:18:17 +0600

    TokenProvider and JWT Authentication filter has been added.

---
+++ bend-library/config/security/jwt/src/main/java/bend/library/config/security/jwt/filter/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,16 @@
+package bend.library.config.security.jwt.filter.jwt;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author lemon
+ * Email lemon.bashar@gmail.com
+ * Created 2/13/2020
+ */
+public class JwtAuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+}
---
+++ bend-library/config/security/jwt/src/main/java/bend/library/config/security/jwt/jwt/TokenProvider.java
@@ -0,0 +1,9 @@
+package bend.library.config.security.jwt.jwt;
+
+/**
+ * @author lemon
+ * Email lemon.bashar@gmail.com
+ * Created 2/13/2020
+ */
+public class TokenProvider {
+}
