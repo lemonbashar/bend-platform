7531ac4faded5c345612f42f48e9ee0912e5f9c3
lemon <lemon.bashar@gmail.com>
2020-03-09 22:36:26 +0600

    Merge branch 'v-1.0.0' of https://bitbucket.org/lemon_spring/bend-platform into v-1.0.0
    # Conflicts:
    #	bend-library/config/database/rdbms/src/main/java/bend/library/config/database/rdbms/RdbmsJpaConfig.java

--- bend-library/config/database/rdbms/src/main/java/bend/library/config/database/rdbms/RdbmsJpaConfig.java
+++ bend-library/config/database/rdbms/src/main/java/bend/library/config/database/rdbms/RdbmsJpaConfig.java
@@ -6,7 +6,9 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.*;
 import org.hibernate.cfg.Environment;
+import org.hibernate.type.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -20,6 +22,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.SharedCacheMode;
 import javax.sql.DataSource;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -69,8 +75,105 @@ public class RdbmsJpaConfig {
         em.setPersistenceUnitName(properties.getDatabase().getJpa().getPersistentUnit());
         em.setSharedCacheMode(SharedCacheMode.ALL);
         em.setJpaProperties(hibernateProperties());
+        Map<String, Object> map = new HashMap<>();
+        //map.put("hibernate.ejb.interceptor", interceptor());
+        em.setJpaPropertyMap(map);
         return em;
     }
+
+    /*private Interceptor interceptor() {
+        return new EmptyInterceptor() {
+            @Override
+            public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+                super.onDelete(entity, id, state, propertyNames, types);
+            }
+
+            @Override
+            public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+                return super.onFlushDirty(entity, id, currentState, previousState, propertyNames, types);
+            }
+
+            @Override
+            public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+                return super.onLoad(entity, id, state, propertyNames, types);
+            }
+
+            @Override
+            public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
+                return super.onSave(entity, id, state, propertyNames, types);
+            }
+
+            @Override
+            public void postFlush(Iterator entities) {
+                super.postFlush(entities);
+            }
+
+            @Override
+            public void preFlush(Iterator entities) {
+                super.preFlush(entities);
+            }
+
+            @Override
+            public Boolean isTransient(Object entity) {
+                return super.isTransient(entity);
+            }
+
+            @Override
+            public Object instantiate(String entityName, EntityMode entityMode, Serializable id) {
+                return super.instantiate(entityName, entityMode, id);
+            }
+
+            @Override
+            public int[] findDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+                return super.findDirty(entity, id, currentState, previousState, propertyNames, types);
+            }
+
+            @Override
+            public String getEntityName(Object object) {
+                return super.getEntityName(object);
+            }
+
+            @Override
+            public Object getEntity(String entityName, Serializable id) {
+                return super.getEntity(entityName, id);
+            }
+
+            @Override
+            public void afterTransactionBegin(Transaction tx) {
+                super.afterTransactionBegin(tx);
+            }
+
+            @Override
+            public void afterTransactionCompletion(Transaction tx) {
+                super.afterTransactionCompletion(tx);
+            }
+
+            @Override
+            public void beforeTransactionCompletion(Transaction tx) {
+                super.beforeTransactionCompletion(tx);
+            }
+
+            @Override
+            public String onPrepareStatement(String sql) {
+                return super.onPrepareStatement(sql);
+            }
+
+            @Override
+            public void onCollectionRemove(Object collection, Serializable key) throws CallbackException {
+                super.onCollectionRemove(collection, key);
+            }
+
+            @Override
+            public void onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
+                super.onCollectionRecreate(collection, key);
+            }
+
+            @Override
+            public void onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
+                super.onCollectionUpdate(collection, key);
+            }
+        };
+    }*/
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
