891ae09db58bc680e4cb03b808f52a626d6d1bce
lemon <lemon.bashar@gmail.com>
2020-03-11 02:06:45 +0600

    Test cases and project optimized.

---
+++ bend-library/config/src/main/java/bend/library/config/WebConfigurer.java
@@ -0,0 +1,51 @@
+package bend.library.config;
+
+import bend.framework.properties.springproperties.SpringProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*TODO:ADD ONLY AUTHORIZED CORS-ORIGIN URL LIKE: .allowedOrigins("http://localhost:4200","http://192.168.0.153:4200") AND
+* config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("http://192.168.0.153:4200");
+*
+* */
+@EnableWebMvc
+@RequiredArgsConstructor
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    private final SpringProperties properties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+                .allowedHeaders("*").allowedOrigins("*")
+                //.allowedOrigins(properties.getSettings().getSecurity().getCors().getAllowedClients()) /*FOR ALL CLIENT URL*/
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        for (String allowedClient : properties.getSettings().getSecurity().getCors().getAllowedClients())
+            config.addAllowedOrigin(allowedClient);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
