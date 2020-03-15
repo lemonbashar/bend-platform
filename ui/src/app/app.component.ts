06671724ba5105f4a15f36b14019a8bec66b990e
lemon <lemon.bashar@gmail.com>
2020-03-16 00:02:02 +0600

    UI part moved from Previous v-1.0.8 of spring boot project. But not sync with REST-APIS.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Microservice';
+  title = 'Bend-Platform';
 
   ROLE_FOR_ADMIN = ['ROLE_ADMIN'];
 }
