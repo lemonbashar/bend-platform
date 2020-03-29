d3a8b14d68e6ba3a8c0606ef8bf2e014a49b08a9
lemonbashar <lemon.bashar@gmail.com>
2020-03-29 19:30:25 +0600

    Micro-Service Successfully Run on this Configurations.

--- bend-library/domain/cluster/src/test/java/bend/library/domain/cluster/service/DatabaseConfigServiceTest.java
+++ bend-library/domain/cluster/src/test/java/bend/library/domain/cluster/service/DatabaseConfigServiceTest.java
@@ -1,12 +1,11 @@
 package bend.library.domain.cluster.service;
 
 import bend.library.config.PropertiesConfig;
-import bend.library.constant.ProfileConstants;
 import bend.library.config.database.rdbms.RdbmsJpaConfig;
-import bend.library.config.security.SecurityConfig;
 import bend.library.config.security.data.LoginInfo;
-import bend.library.config.security.jwt.JwtSecurityConfig;
 import bend.library.config.security.service.AuthenticationService;
+import bend.library.constant.ProfileConstants;
+import bend.library.core.CoreConfig;
 import bend.library.core.prepersist.PrePersistConfiguration;
 import bend.library.domain.cluster.ClusterDomainConfig;
 import bend.library.domain.cluster.repositories.DatabaseConfigRepository;
@@ -20,7 +19,7 @@ import org.springframework.test.context.TestPropertySource;
 @Tag(ProfileConstants.TestInclude.RUN_FLUENTLY_WITHOUT_DB_DEPENDENCY)
 @ActiveProfiles(profiles = "test")
 @TestPropertySource(locations = "classpath:config/application-test.yml")
-@SpringBootTest(classes = {PropertiesConfig.class, RdbmsJpaConfig.class, ClusterDomainConfig.class, PrePersistConfiguration.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(classes = {PropertiesConfig.class, RdbmsJpaConfig.class, ClusterDomainConfig.class, CoreConfig.class, PrePersistConfiguration.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class DatabaseConfigServiceTest {
     @Autowired
     private DatabaseConfigService databaseConfigService;
