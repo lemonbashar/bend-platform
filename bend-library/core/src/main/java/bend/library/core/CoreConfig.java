b7e01139a673e63763e7601c3f420bd071b03481
lemonbashar <lemon.bashar@gmail.com>
2020-03-29 19:26:19 +0600

    Core Config has been added.

---
+++ bend-library/core/src/main/java/bend/library/core/CoreConfig.java
@@ -0,0 +1,9 @@
+package bend.library.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("bend.library.core")
+public class CoreConfig {
+}
