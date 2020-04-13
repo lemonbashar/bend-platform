8b42132b3f111318440ed86fd0fd1eca80111891
lemon <lemon.bashar@gmail.com>
2020-04-13 22:48:14 +0600

    Common Css put outside on project and init Ficket App..

--- ui/projects/bend-ficket-ui/src/app/app.component.ts
+++ ui/projects/bend-ficket-ui/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ficket-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'bend-ficket-ui';
