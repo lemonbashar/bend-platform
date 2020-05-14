030456278b235115e953fd7afdc19c7b7a56342c
lemon <lemon.bashar@gmail.com>
2020-05-14 23:07:05 +0600

    LocaleResolver has been added.

---
+++ bend-library/config/src/main/java/bend/library/config/locale/I18nL10nConfigBeans.java
@@ -0,0 +1,29 @@
+package bend.library.config.locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class I18nL10nConfigBeans {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+        acceptHeaderLocaleResolver.setDefaultLocale(Locale.US);
+        return acceptHeaderLocaleResolver;
+    }
+
+    @Bean
+    ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return  messageSource;
+    }
+}
